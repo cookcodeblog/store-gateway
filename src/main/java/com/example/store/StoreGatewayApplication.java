371d6c377f287a4a594482ffc4d55a0d883041c8
Lin Sirui <lsirui@redhat.com>
2020-12-24 13:16:42 +0800

    Zuul without Eureka

--- src/main/java/com/example/store/StoreGatewayApplication.java
+++ src/main/java/com/example/store/StoreGatewayApplication.java
@@ -2,12 +2,11 @@ package com.example.store;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableZuulProxy
-@EnableEurekaClient
+//@EnableEurekaClient
 public class StoreGatewayApplication {
 
     public static void main(String[] args) {
